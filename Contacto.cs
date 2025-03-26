a66f446f606654d72290bde9982d2191b68d5328
Keren <131375714+KerenRivera@users.noreply.github.com>
2025-03-26 09:29:07 -0400

    Class 'Contact' created

---
+++ Contacto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts_project
+{
+     class Contacto
+     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public Contacto(int id, string name, string phone, string email, string address)
+        {
+            Id = id;
+            Name = name;
+            Phone = phone;
+            Email = email;
+            Address = address;  
+        }
+
+        public void ViewContact()
+        {
+            Console.WriteLine($"{Id}    {Name}      {Phone}      {Email}     {Address}");
+        }
+     }
+    
+
+}
